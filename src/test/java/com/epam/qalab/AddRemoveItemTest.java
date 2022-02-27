9dacbe884b097f76d9ad5bc941bce53196981a44
dominikb <bodzioch.dominik@wp.pl>
2022-02-28 00:57:08 +0100

    refactoring & clean code

--- src/test/java/com/epam/qalab/AddRemoveItemTest.java
+++ src/test/java/com/epam/qalab/AddRemoveItemTest.java
@@ -1,80 +1,106 @@
 package com.epam.qalab;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import org.testng.asserts.SoftAssert;
 import java.time.Duration;
 
 public class AddRemoveItemTest {
+    private WebDriver driver;
 
-    @Test
-    public void addItemToCartTest() {
-        WebDriverManager.chromedriver().browserVersion("98").setup();
-        WebDriver driver = new ChromeDriver();
+    @BeforeTest
+    public void beforeTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
         driver.get("https://www.amazon.com/");
-
-        String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://www.amazon.com/", "The current link is not as expected");
-
-        WebElement anyCategory = driver.findElement(By.xpath("(//span[@class='a-size-small a-color-base truncate-2line'])[2]"));
-        anyCategory.click();
-
-        WebElement anyItem = driver.findElement(By.xpath("(//img[@class=\"s-image\"])[1]"));
-        anyItem.click();
-
-        WebElement addToCartButton = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
-        addToCartButton.click();
-
-        WebElement addedToCart = driver.findElement(By.xpath("//span[contains(text(), 'Added to Cart')]"));
-        Assert.assertEquals(addedToCart.getText(), "Added to Cart", "Adding to Cart doesn't work properly");
-
-        WebElement navCartCount = driver.findElement(By.xpath("//span[@id='nav-cart-count' and contains(text(), '1')]"));
-        Assert.assertEquals(navCartCount.getText(), "1", "Wrong number of items in the Cart");
-
-        driver.close();
-        driver.quit();
     }
 
     @Test
-    public void removeItemFromCartTest() {
-        WebDriverManager.chromedriver().browserVersion("98").setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
+    public void shouldAddAnyItemToCart() {
+        SoftAssert softAssert = new SoftAssert();
+        chooseAnyCategory();
+        chooseAnyItem();
+        addItemToCart();
+        softAssert.assertEquals(getAddedToCartInfo(), "Added to Cart", "Adding to Cart doesn't work properly");
+        softAssert.assertEquals(getNavCartCount(), "1", "Wrong number of items in the Cart");
+        softAssert.assertAll();
+    }
 
-        String currentUrl = driver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, "https://www.amazon.com/", "The current link is not as expected");
+    @Test
+    public void shouldRemoveItemFromCart() {
+        SoftAssert softAssert = new SoftAssert();
+        chooseAnyCategory();
+        chooseAnyItem();
+        addItemToCart();
+        openCart();
+        deleteItemFromCart();
+        softAssert.assertEquals(getHeadingInfo(), "Your Amazon Cart is empty.", "Your Amazon Cart is not empty or not available");
+        softAssert.assertEquals(getPrice(), "0.00", "Subtotal is not '0.00'");
+        softAssert.assertAll();
+    }
 
-        WebElement anyCategory = driver.findElement(By.xpath("(//span[@class='a-size-small a-color-base truncate-2line'])[2]"));
+    @AfterTest
+    public void afterTest() {
+        driver.quit();
+    }
+
+    private void chooseAnyCategory() {
+        WebElement anyCategory = driver.findElement(By.xpath("//span[@class='a-size-small a-color-base truncate-2line']"));
         anyCategory.click();
+    }
 
-        WebElement anyItem = driver.findElement(By.xpath("(//img[@class=\"s-image\"])[1]"));
+    private void chooseAnyItem() {
+        WebElement anyItem = driver.findElement(By.xpath("//img[@class=\"s-image\"]"));
         anyItem.click();
+    }
 
+    private void addItemToCart() {
         WebElement addToCartButton = driver.findElement(By.xpath("//input[@id='add-to-cart-button']"));
         addToCartButton.click();
+    }
 
-        WebElement basketButton = driver.findElement(By.xpath("//span[@id='nav-cart-count']"));
-        basketButton.click();
+    private String getAddedToCartInfo() {
+        return new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//span[contains(text(), 'Added to Cart')]"))).getText();
+    }
 
+    private String getNavCartCount() {
+        return driver.findElement(By.xpath("//span[@id='nav-cart-count' and contains(text(), '1')]")).getText();
+    }
+
+    private void deleteItemFromCart() {
         WebElement deleteButton = driver.findElement(By.xpath("//input[@data-action='delete']"));
         deleteButton.click();
+    }
 
-        WebElement heading = new WebDriverWait(driver, Duration.ofSeconds(3))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class='a-spacing-mini a-spacing-top-base']")));
-        Assert.assertEquals(heading.getText(), "Your Amazon Cart is empty.", "Your Amazon Cart is not empty or not available");
+    private void openCart() {
+        WebElement cartButton = driver.findElement(By.xpath("//span[@id='nav-cart-count']"));
+        cartButton.click();
+    }
 
-        WebElement currencyPrice = driver.findElement(By.xpath("//span [@class='a-size-medium a-color-base sc-price sc-white-space-nowrap']"));
-        String price = currencyPrice.getText().trim().replaceFirst("\\D+", "").replace(",", ".");
-        Assert.assertEquals(price, "0.00", "Subtotal is not '0.00'");
+    private String getHeadingInfo() {
+        return new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h1[@class='a-spacing-mini a-spacing-top-base']"))).getText();
+    }
 
-        driver.close();
-        driver.quit();
+    private String getPrice() {
+        WebElement currencyPrice = driver.findElement(
+                By.xpath("//span [@class='a-size-medium a-color-base sc-price sc-white-space-nowrap']"));
+        return currencyPrice.getText().trim().replaceFirst("\\D+", "").replace(",", ".");
     }
 }

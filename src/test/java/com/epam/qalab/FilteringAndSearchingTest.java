28535593a9cc46df70938e4d0e83013581b84050
Agnieszka Czajka <arz2010@wp.pl>
2022-03-08 23:07:06 +0100

    rewrite tests to pop

--- src/test/java/com/epam/qalab/FilteringAndSearchingTest.java
+++ src/test/java/com/epam/qalab/FilteringAndSearchingTest.java
@@ -1,91 +1,55 @@
 package com.epam.qalab;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import com.epam.qalab.pageObject.pages.CategoryPage;
+import com.epam.qalab.pageObject.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class FilteringAndSearchingTest {
-
-    WebDriver driver;
-
-    @BeforeTest
-    public void beforeTest() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-    }
+public class FilteringAndSearchingTest extends BaseTest {
 
     @BeforeMethod
-    public void setUp() {
-        driver.get("https://www.amazon.com/");
+    public CategoryPage openCategoryPage() {
+        HomePage homePage = new HomePage(webDriver);
+        setUpDriver();
+        return homePage.open().choseCategory();
     }
 
     @AfterTest
     public void afterTest() {
-        driver.quit();
+        quit();
     }
 
     @Test
     public void successfulFilteredByFeaturedBrand() {
 
-        choseCategory();
+        var categoryPage = openCategoryPage();
+        String nameFeaturedBrand = categoryPage.getNameFeaturedBrand();
+        categoryPage = categoryPage
+                .selectFeaturedBrand();
 
-        WebElement anyFeaturedBrand = driver.findElement(By.xpath("//*[text()=\"Featured Brands\"]/parent::*/parent::*/ul/li[1]//a/span"));
-        String nameFeaturedBrand = anyFeaturedBrand.getAttribute("textContent");
-        anyFeaturedBrand.click();
-
-        List<WebElement> productsDescription = driver.findElements(By.xpath("//*[contains(@cel_widget_id,\"RESULTS\")]//h2"));
-
-        Assert.assertTrue(productsDescription.stream().allMatch(item -> item.getText().contains(nameFeaturedBrand)));
+        Assert.assertTrue(categoryPage.getProductsDescription().stream().allMatch(item -> item.getText().contains(nameFeaturedBrand)));
     }
 
     @Test
     public void successfulFilteringByPrice() {
 
-        choseCategory();
+        ArrayList<Double> elementsPrices = openCategoryPage()
+                .selectPriceRangeOption()
+                .getPricesOfDisplayedElements();
 
-        WebElement priceRangeOption = driver.findElement(By.xpath("//*[text()='Price']/parent::*/following-sibling::ul/li[1]//a"));
-        priceRangeOption.click();
-
-        List<WebElement> productsPrices = driver.findElements(By.xpath("//*[@class=\"a-price\"]/span[1]"));
-        ArrayList<Double> prices = new ArrayList<>();
-
-        for (WebElement product:productsPrices) {
-            prices.add(Double.parseDouble(product.getAttribute("textContent").substring(1)));
-        }
-        Assert.assertTrue(prices.stream().allMatch(priceElem -> priceElem <= 25));
+        Assert.assertTrue(elementsPrices.stream().allMatch(priceElem -> priceElem <= 25));
     }
 
     @Test
     public void successfulSortingDescendingWithinCategory() {
 
-        choseCategory();
+        CategoryPage categoryPage = openCategoryPage()
+                .showAllResult()
+                .openSortDropDown()
+                .sortHighToLowPrice();
 
-        WebElement btnSeeAllResults = driver.findElement(By.linkText("See all results"));
-        btnSeeAllResults.click();
-
-        WebElement btnDropDown = driver.findElement(By.xpath("//*[contains(@class,\"a-button-dropdown\")]/span"));
-        btnDropDown.click();
-
-        WebElement optionHighToLowPrice = driver.findElement(By.linkText("Price: High to Low"));
-        optionHighToLowPrice.click();
-
-        List<WebElement> productsPrices = driver.findElements(By.xpath("//*[@class=\"a-price\"]/span[1]"));
-
-        Double firstElemPrice = Double.parseDouble(productsPrices.get(0).getAttribute("textContent").substring(1));
-        Double secondElemPrice = Double.parseDouble(productsPrices.get(1).getAttribute("textContent").substring(1));
-
-        Assert.assertTrue(firstElemPrice > secondElemPrice);
-    }
-
-    private void choseCategory() {
-        WebElement categoryLink = driver.findElement(By.xpath("(//*[@class=\"a-link-normal quadrant-overlay\"])[1]"));
-        categoryLink.click();
+        Assert.assertTrue(categoryPage.getElementPrice(0) > categoryPage.getElementPrice(1));
     }
 }

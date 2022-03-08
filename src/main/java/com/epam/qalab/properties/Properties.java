4064033594a0b02ffdb6d2a7d737c398f54fabfc
Kristina K. Peneva <85350642+kristinapeneva@users.noreply.github.com>
2022-03-08 19:59:14 +0100

    delete Properties.java (not needed)

--- src/main/java/com/epam/qalab/properties/Properties.java
+++
@@ -1,6 +0,0 @@
-package com.epam.qalab.properties;
-
-public class Properties {
-    public static final String GIT_HUB_LOGIN = "julia070796@gmail.com";
-    public static final String GIT_HUB_PASS = "";
-}

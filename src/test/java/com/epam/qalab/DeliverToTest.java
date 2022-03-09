f790c3fe1e78df0a1dd635c5035ec86f19104427
Kristina K. Peneva <85350642+kristinapeneva@users.noreply.github.com>
2022-03-09 11:24:57 +0100

    Tests merged to one file - DeliverToTest

--- src/test/java/com/epam/qalab/DeliverToTest.java
+++ src/test/java/com/epam/qalab/DeliverToTest.java
@@ -1,111 +1,65 @@
 package com.epam.qalab;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.epam.qalab.pageObject.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 
-public class DeliverToTest {
-    WebDriver driver;
+public class DeliverToTest extends BaseTest {
 
-    @BeforeTest
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.amazon.com/");
+    @Test
+    public void verifyZipCode() {
+        String zipCode = "37219";
+        HomePage homePage = new HomePage(webDriver);
+        String newLocation = homePage
+                .open()
+                .deliverToButtonClick()
+                .inputZipCode(zipCode)
+                .applyButtonClick()
+                .continueButtonClick()
+                .refreshPage()
+                .getNewLocation();
+
+        Assert.assertTrue(newLocation.contains("Nashville 37219"), "The 'Deliver to' location isn't updated.");
     }
 
     @Test
-    public void verifyZipCodeUpdate() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement oldLocation = driver.findElement(By.id("glow-ingress-line2"));
+    public void polandIsPresent() {
+        HomePage homePage = new HomePage(webDriver);
+        List<String> listOfCountries = homePage
+                .open()
+                .deliverToButtonClick()
+                .getListOfCountries();
 
-        deliverToButtonClick();
-
-        WebElement zipCodeField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("GLUXZipUpdateInput")));
-        zipCodeField.sendKeys("37219");
-
-        WebElement applyButton = driver.findElement(By.xpath("//*[@data-action='GLUXPostalUpdateAction']"));
-        applyButton.click();
-
-        WebElement continueButton = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='a-popover-footer']//input[@id='GLUXConfirmClose']")));
-        continueButton.click();
-
-        wait.until(ExpectedConditions.stalenessOf(oldLocation));
-        String actualLocation = driver.findElement(By.id("glow-ingress-line2")).getText();
-
-        Assert.assertTrue(actualLocation.contains("Nashville 37219"), "The 'Deliver to' location isn't updated.");
+        Assert.assertTrue(listOfCountries.contains("Poland"), "The list of countries doesn't contain Poland.");
     }
 
     @Test
-    public void verifyPolandIsPresent() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        deliverToButtonClick();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='a-dropdown-container']")));
-        Select select = new Select(driver.findElement(By.xpath("//select[@class='a-native-dropdown']")));
-        List<String> list = new ArrayList<>();
-        for (WebElement element: select.getOptions()) {
-            list.add(element.getText());
-        }
-
-        Assert.assertTrue(list.contains("Poland"), "The list of countries doesn't contain Poland.");
-    }
-
-    @Test
-    public void verifyShippingToCountry() {
+    public void shippingToCountry() {
         String country = "Bulgaria";
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement oldLocation = driver.findElement(By.id("glow-ingress-line2"));
+        HomePage homePage = new HomePage(webDriver);
+        String shippingCountry = homePage
+                .open()
+                .deliverToButtonClick()
+                .selectCountry(country)
+                .doneButtonClick()
+                .refreshPage()
+                .choosingCategory()
+                .choosingItem()
+                .getShippingCountry();
 
-        deliverToButtonClick();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='a-dropdown-container']")));
-        Select select = new Select(driver.findElement(By.xpath("//select[@class='a-native-dropdown']")));
-        select.selectByVisibleText(country);
-
-        WebElement done = driver.findElement(By.xpath("//button[@name='glowDoneButton']"));
-        done.click();
-
-        wait.until(ExpectedConditions.stalenessOf(oldLocation));
-        WebElement category = driver.findElement(By.xpath("//a[@aria-label='Keyboards']"));
-        category.click();
-
-        WebElement keyboardItem = wait
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@class,'s-main-slot ')]/div[@data-component-type='s-search-result'][1]//h2/a")));
-        keyboardItem.click();
-
-        String depositToInfo = driver
-                .findElement(By.xpath("//*[@id='exports_desktop_qualifiedBuybox_tlc_feature_div']/span[@class='a-size-base a-color-secondary']")).getText();
-        Assert.assertTrue(depositToInfo.contains(country), "The chosen shipping country isn't updated.");
-    }
-
-    private void deliverToButtonClick() {
-        WebElement deliverToButton = driver.findElement(By.id("nav-global-location-popover-link"));
-        deliverToButton.click();
+        Assert.assertTrue(shippingCountry.contains(country), "The chosen shipping country isn't updated.");
     }
 
     @AfterMethod
     public void preparePage() {
-        driver.navigate().refresh();
+        webDriver.navigate().refresh();
     }
 
     @AfterTest
     public void tearDown() {
-        driver.quit();
+        quit();
     }
 }

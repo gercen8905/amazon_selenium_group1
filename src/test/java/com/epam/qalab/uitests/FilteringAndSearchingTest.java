df70e53094efaf2a305838361446bd56ad84efd4
Agnieszka Czajka <arz2010@wp.pl>
2022-03-23 18:56:05 +0100

    FilteringAndSearchingTest: changed method before and some refactor

--- src/test/java/com/epam/qalab/uitests/FilteringAndSearchingTest.java
+++ src/test/java/com/epam/qalab/uitests/FilteringAndSearchingTest.java
@@ -3,18 +3,20 @@ package com.epam.qalab.uitests;
 import com.epam.qalab.BaseTest;
 import com.epam.qalab.pageObject.pages.CategoryPage;
 import com.epam.qalab.pageObject.pages.HomePage;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class FilteringAndSearchingTest extends BaseTest {
+    private CategoryPage categoryPage;
 
     @BeforeMethod
-    public CategoryPage openCategoryPage() {
+    public void openCategoryPage() {
         HomePage homePage = new HomePage(webDriver);
         setUpDriver();
-        return homePage.open().choseCategory();
+        categoryPage = homePage.open().choseCategory();
     }
 
     @AfterTest
@@ -25,32 +27,36 @@ public class FilteringAndSearchingTest extends BaseTest {
     @Test
     public void successfulFilteredByFeaturedBrand() {
 
-        var categoryPage = openCategoryPage();
         String nameFeaturedBrand = categoryPage.getNameFeaturedBrand();
-        categoryPage = categoryPage
-                .selectFeaturedBrand();
+        List<WebElement> productsDescriptions = categoryPage
+                .selectFeaturedBrand()
+                .getProductsDescription();
 
-        Assert.assertTrue(categoryPage.getProductsDescription().stream().allMatch(item -> item.getText().contains(nameFeaturedBrand)));
+        Assert.assertTrue(productsDescriptions.stream().allMatch(item -> item.getText().contains(nameFeaturedBrand)),
+                "Not each description contains featured brand");
     }
 
     @Test
     public void successfulFilteringByPrice() {
+        // this test check only the main price, when there is an option with lower price test will be failed
 
-        ArrayList<Double> elementsPrices = openCategoryPage()
+        List<Double> elementsPrices = categoryPage
                 .selectPriceRangeOption()
                 .getPricesOfDisplayedElements();
 
-        Assert.assertTrue(elementsPrices.stream().allMatch(priceElem -> priceElem <= 25));
+        Assert.assertTrue(elementsPrices.stream().allMatch(priceElem -> priceElem <= 25),
+                "Not each price is <= 25");
     }
 
     @Test
     public void successfulSortingDescendingWithinCategory() {
 
-        CategoryPage categoryPage = openCategoryPage()
+        categoryPage
                 .showAllResult()
                 .openSortDropDown()
                 .sortHighToLowPrice();
 
-        Assert.assertTrue(categoryPage.getElementPrice(0) > categoryPage.getElementPrice(1));
+        Assert.assertTrue(categoryPage.getElementPrice(0) > categoryPage.getElementPrice(1),
+                "Sorting doesn't work correctly");
     }
 }
